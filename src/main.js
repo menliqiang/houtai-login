84fc076be6651f38956cc1922aeadbfc36f81de7
men liqiang <3876744@qq.com>
2018-07-09 15:17:13 +0800

    登陆首页显示

--- src/main.js
+++ src/main.js
@@ -5,8 +5,22 @@ import App from './App';
 import router from './router';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+import '@/assets/css/style.css';
+import MyAxios from '@/plugins/MyAxios';
+import moment from 'moment';
+
 Vue.config.productionTip = false;
+
+// 全局过滤器，格式化日期
+Vue.filter('fmtDate', (value, fmtString) => {
+  return moment(value).format(fmtString);
+});
+
+// 注册MyAxios插件
+Vue.use(MyAxios);
+// 注册ElementUI插件
 Vue.use(ElementUI);
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
